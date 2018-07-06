3450062d8b2a49a2d3ae3c44775ec79e1e8574cc
Senzi <zwane.senzi05@gmail.com>
2018-07-06 09:03:48 +0200

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WinnerPage } from '../winner/winner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+scoreTeamA=0;
+scoreTeamB=0;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+ teamA(value:number):void{
+this.scoreTeamA +=value;
+  }
+teamB(value:number):void{
+    this.scoreTeamB +=value; 
+   }
+Rest(value:number):void{
+  this.scoreTeamA = value;
+  this.scoreTeamB = value;
+}
+endMatch():void{ 
+this.navCtrl.push(WinnerPage)
+}
 }
